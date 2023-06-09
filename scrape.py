2ed2d8e09362e65a432f442a5c9450fcb1bc73d9
xanderabbott <ada5@rice.edu>
2023-06-08 18:58:51 -0700

    extracted player row

--- scrape.py
+++ scrape.py
@@ -1,9 +1,29 @@
 # imports
 import pandas as pd
 import numpy as np
-import selenium as se
+from bs4 import BeautifulSoup
 import sklearn
 import seaborn as sns
+import requests
+import re
 
-def extract(player):
-    pass
+url = "https://www.fantasypros.com/nfl/stats/"
+
+def extract(position : str, player : str, year : str, scoring : str):
+    """
+    Reads html from fantasypros stats per position, year, and scoring specifications
+    Inputs: 
+        Position: a string consisting of "wr", "rb", "te", "qb"
+        Year: string consisting of a year
+        Scoring: string consisting of "PPR", "HALF"
+    """
+    url = "https://www.fantasypros.com/nfl/stats/" + position + ".php?year=" + year + "&scoring=" + scoring
+    # access url
+    result = requests.get(url)
+    # read html
+    doc = BeautifulSoup(result.text, "html.parser")
+    # tr is table row. Find all table rows with class starting with "mpb-player-..."
+    # each result is a player and their stats
+    rows = doc.find_all("tr", attrs={'class': re.compile('mpb\-player\-*')}, limit=1)
+    return rows
+print(extract("wr", "_", "2022", "PPR"))
